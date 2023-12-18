03f9104b44b95a8478d0085238674f84b832ebbf
Ahmet <ahmettgrbzz@gmail.com>
2023-12-18 22:09:15 +0300

    Latest

---
+++ 03-Flask-Cloudformation-Roman-Numerals-Converter/Cli.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+dnf update -y
+dnf install python3 -y
+dnf install python3-pip -y
+pip3 install flask
+cd /home/ec2-user
+wget https://raw.githubusercontent.com/ahmettgg/My_Project/main/03-Flask-Cloudformation-Roman-Numerals-Converter/roman-numerals-converter-app.py
+mkdir templates && cd /home/ec2-user/templates
+wget https://raw.githubusercontent.com/ahmettgg/My_Project/main/03-Flask-Cloudformation-Roman-Numerals-Converter/templates/index.html
+wget https://raw.githubusercontent.com/ahmettgg/My_Project/main/03-Flask-Cloudformation-Roman-Numerals-Converter/templates/result.html
+cd ..
+python3 roman-numerals-converter-app.py
