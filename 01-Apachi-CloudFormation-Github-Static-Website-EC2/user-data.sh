31614c5bd4a2b8ea22bd4bf0b9df0f6df69224f9
Ahmet <ahmettgrbzz@gmail.com>
2024-04-04 19:43:28 +0300

    update

--- 01-Apachi-CloudFormation-Github-Static-Website-EC2/user-data.sh
+++ 01-Apachi-CloudFormation-Github-Static-Website-EC2/user-data.sh
@@ -1,8 +1,9 @@
 #! /bin/bash
 yum update -y
 yum install httpd -y
-FOLDER="https://raw.githubusercontent.com/ahmettgg/My_Project/main/01-Kittens-Carousel-Static-Website-EC2/static-web"
+FOLDER="https://raw.githubusercontent.com/ahmettgg/AWS_My_Project/main/01-Apachi-CloudFormation-Github-Static-Website-EC2/static-web"
 cd /var/www/html
+rm -f index.html
 wget ${FOLDER}/index.html
 wget ${FOLDER}/cat0.jpg
 wget ${FOLDER}/cat1.jpg
--- 03-Flask-Cloudformation-Roman-Numerals-Converter/Cli.sh
+++ 03-Flask-Cloudformation-Roman-Numerals-Converter/Cli.sh
@@ -1,12 +1,12 @@
-#!/bin/bash
+#! /bin/bash
 dnf update -y
 dnf install python3 -y
 dnf install python3-pip -y
 pip3 install flask
 cd /home/ec2-user
-wget https://raw.githubusercontent.com/ahmettgg/My_Project/main/03-Flask-Cloudformation-Roman-Numerals-Converter/roman-numerals-converter-app.py
+wget https://raw.githubusercontent.com/ahmettgg/AWS_My_Project/main/03-Flask-Cloudformation-Roman-Numerals-Converter/roman-numerals-converter-app.py
 mkdir templates && cd /home/ec2-user/templates
-wget https://raw.githubusercontent.com/ahmettgg/My_Project/main/03-Flask-Cloudformation-Roman-Numerals-Converter/templates/index.html
-wget https://raw.githubusercontent.com/ahmettgg/My_Project/main/03-Flask-Cloudformation-Roman-Numerals-Converter/templates/result.html
+wget https://raw.githubusercontent.com/ahmettgg/AWS_My_Project/main/03-Flask-Cloudformation-Roman-Numerals-Converter/templates/index.html
+wget https://raw.githubusercontent.com/ahmettgg/AWS_My_Project/main/03-Flask-Cloudformation-Roman-Numerals-Converter/templates/result.html
 cd ..
 python3 roman-numerals-converter-app.py
